f8acc46a7d989ca89b820542b499b988b2e81f65
Ghulam-Rasool-Khan <aanarang456@gmail.com>
2025-05-05 08:16:21 +0500

    20

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -111,29 +111,3 @@ export default Contact;
 
 
 
-
-
-// const [result, setResult] = React.useState("");
-
-// const onSubmit = async (event) => {
-//   event.preventDefault();
-//   setResult("Sending....");
-//   const formData = new FormData(event.target);
-
-//   formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
-
-//   const response = await fetch("https://api.web3forms.com/submit", {
-//     method: "POST",
-//     body: formData
-//   });
-
-//   const data = await response.json();
-
-//   if (data.success) {
-//     setResult("Form Submitted Successfully");
-//     event.target.reset();
-//   } else {
-//     console.log("Error", data);
-//     setResult(data.message);
-//   }
-// };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import {motion} from "framer-motion"
 
 function Header() {
   return (
-    <div className=" min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden" style={{ backgroundImage: "url('/header_img.png')" }} id="Header"> 
+    <div className=" min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden" style={{ backgroundImage: "url('./header_img.png')" }} id="Header"> 
       
       <Navbar></Navbar>
 
